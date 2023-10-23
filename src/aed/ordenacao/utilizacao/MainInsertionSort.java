59b04717914baea2e592318e732469472a2d7531
Pedro Pedro <pedro.afonso.pedro.pp@gmail.com>
2023-10-23 12:32:34 +0100

    insertionSort: updated

--- src/aed/ordenacao/utilizacao/MainInsertionSort.java
+++ src/aed/ordenacao/utilizacao/MainInsertionSort.java
@@ -1,2 +1,13 @@
-package aed.ordenacao.utilizacao;public class MainInsertionSort {
+package aed.ordenacao.utilizacao;
+
+import aed.ComparacaoInteiros;
+import aed.ordenacao.algoritmos.InsertionSort;
+import aed.ordenacao.algoritmos.QuickSort;
+
+public class MainInsertionSort {
+
+    public static void main(String[] args) {
+        InsertionSort<Integer> insertionSort = new InsertionSort<>(ComparacaoInteiros.CRITERIO);
+        insertionSort.apresentar(20, 3, 7, 2, 5, 4, 1, 6, 8, 9);
+    }
 }
